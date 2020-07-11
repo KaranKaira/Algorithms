6514a6910900ac21b66fa01dabb26ac98cfc7f65
Karan Kaira <46125192+KaranKaira@users.noreply.github.com>
2020-07-12 01:28:43 +0530

    Create Minnoofinsertionsanddeletion.cpp

---
+++ DynamicProgramming/Minnoofinsertionsanddeletion.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	//code
+	 int n,m;cin>>n>>m;
+	    string a,b;cin>>a>>b;
+	    int dp[n+1][m+1];
+	    memset(dp,0,sizeof(dp));
+	    for(int i = 1;i<=n;i++)
+	    for(int j = 1;j<=m;j++)
+	    if(a[i-1] == b[j-1])
+	    dp[i][j] = 1 + dp[i-1][j-1];
+	    else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+	    
+	    cout<< n + m - 2*dp[n][m] << endl;
+	
+	return 0;
+}
