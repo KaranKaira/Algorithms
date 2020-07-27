859ef63dc0c227e840431de6e1f574a867c0df66
Karan Kaira <46125192+KaranKaira@users.noreply.github.com>
2020-07-27 21:18:41 +0530

    Create reverseLLinsize.cpp

---
+++ LinkedList/reverseLLinsize.cpp
@@ -0,0 +1,31 @@
+
+struct node *reverse (struct node *head, int k)
+{ 
+    // Complete this method
+    if(head == NULL) return head;
+    node * cur = head;
+    node * p = NULL,*n = head->next;
+    int c = 0 ;
+    while(cur != NULL && c < k)
+    {
+        c++;
+        n = cur->next;
+        cur->next = p;
+        p = cur;
+        cur = n;
+
+    }
+    
+    if(n)
+        head->next = reverse(n,k);
+    return p;
+        
+    
+}
+
+int main()
+{
+
+//formalities like creating nodes pushing nodes in LL 
+
+}
