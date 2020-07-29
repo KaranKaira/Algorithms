7eb66d313097f7510bd896efbdaa4dcd445e7836
Karan Kaira <46125192+KaranKaira@users.noreply.github.com>
2020-07-29 18:44:19 +0530

    Create CycleinDG.cpp

---
+++ Graphs/CycleinDG.cpp
@@ -0,0 +1,37 @@
+int dfs(int i,vector<int> adj[],int used[])
+{
+   used[i] =1 ;
+   for(int v:adj[i])
+   {
+       if(used[v] == 1) return 1;
+        else if(used[v]==0) {
+            if(dfs(v,adj,used)) return 1;
+        }
+       
+   }
+   used[i] = 2;
+   return 0;
+}
+bool isCyclic(int v, vector<int> adj[])
+{
+    // Your code here
+    int used[v] ;
+    memset(used,0,sizeof(used));
+    int ok = 0;
+    for(int i =0 ;i<v;i++)
+    if(!used[i])
+    ok = ok || dfs(i,adj,used);
+    return ok;
+    
+}
+
+int main()
+{
+//input graph edges in vector<int> adj[]
+// initialize used/color array with 0  
+int ok = 0
+for(int i = 0;i<numberofvertices;i++)
+if(!used[i]) ok = ok || dfs(i);
+if(ok) cout<<"Cycle present ";
+else cout<<"Cycle Not Present ";
+}
