2c44a6ac3af4238878459bf571dba02df24eae62
Karan Kaira <46125192+KaranKaira@users.noreply.github.com>
2020-07-29 19:02:07 +0530

    Update CycleinUDG.cpp

--- Graphs/CycleinUDG.cpp
+++ Graphs/CycleinUDG.cpp
@@ -1,3 +1,39 @@
 // very similar to finding cycle in Directed Graph
 // in this just the edge connecting parent and child is ignored
 
+int dfs(int i,vector<int> adj[],int used[],int pr[])
+{
+   used[i] =1 ;
+   for(int v:adj[i])
+   {  if(pr[i] == v) continue;
+        pr[v] = i;
+        if(used[v] == 1) return 1;
+        else if(used[v]==0) {
+            if(dfs(v,adj,used,pr)) return 1;
+        }
+       
+   }
+   used[i] = 2;
+   return 0;
+}
+bool isCyclic(int v, vector<int> adj[])
+{
+    // Your code here
+    int used[v] ,pr[v];
+    memset(used,0,sizeof(used));
+  memset(pr,-1,sizeof(pr));
+  
+    int ok = 0;
+    for(int i =0 ;i<v;i++)
+    if(!used[i])
+    ok = ok || dfs(i,adj,used);
+    return ok;
+    
+}
+
+int main()
+{
+//input graph edges in vector<int> adj[]
+// initialize used/color array with 0  
+cout<<isCylic(v,adj[]); // 1 - cycle present 0 - not present 
+}
