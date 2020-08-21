f478633f8d3d1f5828b8266c50aead442c2c81eb
Karan Kaira <46125192+KaranKaira@users.noreply.github.com>
2020-08-21 12:51:10 +0530

    Update WeightedJobScheduling.cpp

--- DynamicProgramming/WeightedJobScheduling.cpp
+++ DynamicProgramming/WeightedJobScheduling.cpp
@@ -11,6 +11,19 @@ int find(job a[],int i)
 for(int j=i-1;j>=0;j--)
     if(a[j].end <= a[i].st) return j;
 return -1;
+    // using binary search
+//     int ans = -1;
+//     int l = 0,r = i-1;
+//     while(l<=r)
+//     {
+//         int mid = l + (r-l)/2;
+//         if(a[mid].end <= a[i].st) {
+//             ans = mid;
+//             l = mid+1;
+//         }
+//         else r = mid-1;
+//     }
+//     return ans;
 }
 class Solution {
     
